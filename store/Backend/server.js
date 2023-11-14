cee2b57ca35c9aa778c3a68bf39b25815121d91c
= <koraydincc@icloud.com>
2023-11-14 22:48:02 +0300

    Example
    Example

--- store/Backend/server.js
+++ store/Backend/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+const mainRoute = require("./routes/index");
+const port = 5000;
+
+//gizli bilgileri gizlemek için dotenv kullandık
+dotenv.config();
+
+//veri tabanı bağlantısı
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Veritabanına bağlanırken hata");
+  }
+};
 
 //req istek response cevap
+
 app.get("/", (req, res) => {
   res.send("Hello Express.js");
 });
@@ -10,6 +28,9 @@ app.get("/api", (req, res) => {
   res.send("Bu API Route");
 });
 
-app.listen(5000, () => {
-  console.log(`Sunucu: ${5000} portunda çalışıyor`);
+app.use("/api", mainRoute);
+
+app.listen(port, () => {
+  connect();
+  console.log(`Sunucu: ${port} portunda çalışıyor`);
 });
